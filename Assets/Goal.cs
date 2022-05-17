1bff5e5398d4fbf326178d76cd832140d8632aea
MSI\xdrag <k021g1163@g.neec.ac.jp>
2022-05-17 15:53:22 +0900

    ボタンを押してリセット、ボタンでシーン遷移、クリアで次シーンへ

--- Assets/Goal.cs
+++ Assets/Goal.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
+    public GameObject clearText;
+    public GameObject nextButton;
+
+
+
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("ÉSÅ[ÉãÅI");
+
+        clearText.SetActive(true);
+        nextButton.SetActive(true);
     }
 }
