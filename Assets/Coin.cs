b550660013e44190a17a372bcab9e01c313d9a03
RyutoOzawa <k021g1163@g.neec.ac.jp>
2022-05-24 15:45:29 +0900

    コイン枚数取得、ビルド

--- Assets/Coin.cs
+++ Assets/Coin.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    public GameManager gameManager;
+
+    public void Start()
+    {
+        GameObject managerObject = GameObject.Find("GameManager");
+        gameManager = managerObject.GetComponent<GameManager>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
+            gameManager.AddCoinCount();
             Destroy(gameObject);
         }
     }
--- Assets/Gamemanager.cs
+++ Assets/Gamemanager.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class Gamemanager : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-public void SceneReset()
+    private int coinCount;
+    public Text textComponent;
+
+    private void Start()
+    {
+        Screen.SetResolution(1920, 1080, false);
+        Application.targetFrameRate = 60;
+        coinCount = 0;
+    }
+
+
+    public void SceneReset()
     {
         string activeSceneName = SceneManager.GetActiveScene().name;
         SceneManager.LoadScene(activeSceneName);
@@ -13,5 +25,12 @@ public void SceneReset()
     public void ChangeScene(string nextScene)
     {
         SceneManager.LoadScene(nextScene);
+    }
+
+    public void AddCoinCount()
+    {
+        coinCount = coinCount + 1;
+        Debug.Log("CoinCount:" + coinCount);
+        textComponent.text = "CoinCount : " + coinCount;
     }
 }
